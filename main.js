f5b543d8bfcd23788f6b1cde3d44a6cd988ef8bf
chidoriii7 <muratore.danilo@yahoo.it>
2024-05-01 03:55:11 +0200

    js-biglietto-treno-form: milestone 1

--- main.js
+++ main.js
@@ -1,32 +1,67 @@
 // -Chiedere all'utente il numero di Km da percorrere.
-const lunghezzaPercorso = parseInt(prompt('Inserisci la lunghezza del percorso in Km:'));
+// const lunghezzaPercorso = parseInt(prompt('Inserisci la lunghezza del percorso in Km:'));
 // -Chiedere L'età del passeggero.
-const etaPasseggero = parseInt(prompt('Inserisci età passeggero: '));
+// const etaPasseggero = parseInt(prompt('Inserisci età passeggero: '));
 // -Calcolo prezzo totale del viaggio secondo le regole 
 
 // -Prezzo base = (0.21 € al km)
-const prezzoBase = lunghezzaPercorso * 0.21;
-let sconto = 0;
 
-//     -SE minorenne = sconto 20%
-if(etaPasseggero < 18) {
 
-    sconto = prezzoBase * 0.2;
-    console.log('sconto del 20% applicato, il totale è:');
+//input
+const pathingInput= document.getElementById('pathing');
+const ageInput = document.getElementById('age');
 
-    //     -SE over 65 = sconto 40%
-}else if (etaPasseggero > 65) {
+//bottoni
+const buttonReadText = document.getElementById('read-text');
+const buttonReset = document.getElementById('reset-button');
+
+//  const pathingInputValue = document.getElementById('pathing').value;
+//  const ageInputValue = document.getElementById('age').value;
+
+ 
+ buttonReadText.addEventListener('click' , function(){
+
+    
+
+    console.log(pathingInput.value , ageInput.value)
+    
+ }) 
+
+
+ 
+
+// const prezzoBase = lunghezzaPercorso * 0.21;
+// let sconto = 0;
+
+// if(etaPasseggero < 18) {
+
+//     sconto = prezzoBase * 0.2;
+//     console.log('sconto del 20% applicato, il totale è:');
+
+   
+// }else if (etaPasseggero > 65) {
      
-    sconto = prezzoBase * 0.4;
-    console.log('sconto del 40% applicato, il totale è:');
+//     sconto = prezzoBase * 0.4;
+//     console.log('sconto del 40% applicato, il totale è:');
 
-}else {
+// }else {
 
-    console.log('nessuno sconto applicato, il totale è:');
+//     console.log('nessuno sconto applicato, il totale è:');
+// 20
+// }
 
-}
-//     -SE non rientra in queste due categorie prezzo base
 
-const prezzoTotale = prezzoBase - sconto
-// -Output del prezzo finale (max 2 decimali)
-console.log (prezzoTotale.toFixed(2))
+// const buttonReadText = document.querySelector('.read-text')
+
+
+// console.dir(buttonReadText)
+
+// buttonReadText.addEventListener('click', function () {
+    
+
+// })
+
+// const prezzoTotale = prezzoBase - sconto
+
+// console.log (prezzoTotale.toFixed(2), '€')
+
